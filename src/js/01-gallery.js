cfbc4a14b561658846e0eb52fd0fd7686bca35b9
Tetiana Tsar <protetsar@gmail.com>
2023-01-09 14:26:40 +0200

    complete tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,17 +1,16 @@
-// Add imports above this line
-// import { galleryItems } from './gallery-items';
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-// import makeGalleryItemsMarkup from './makeGalleryItemsMarkup';
-// import insertMarkup from './insertMarkup';
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import makeGalleryItemsMarkup from './makeGalleryItemsMarkup';
+import insertMarkup from './insertMarkup';
 
-// const galleryRef = document.querySelector('.gallery')
+const galleryRef = document.querySelector('.gallery')
 
-// const result = makeGalleryItemsMarkup (galleryItems)
-// insertMarkup(galleryRef, result)
+const result = makeGalleryItemsMarkup (galleryItems)
+insertMarkup(galleryRef, result)
 
-// let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
-// gallery.on();
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250});
+gallery.on();
 
 
 
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,19 @@
-//  import Player from '@vimeo/player';
-//  import throttle from 'lodash.throttle';
+ import Player from '@vimeo/player';
+ import throttle from 'lodash.throttle';
 
-//  const CURRENT_TIME_KEY = "videoplayer-current-time";
-
-
-// const playerEl = document.getElementById('vimeo-player');
-// const player = new Player(playerEl);
-
-// player.on('timeupdate', throttle(saveCurrentTime, 1000));
-// player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
+ const CURRENT_TIME_KEY = "videoplayer-current-time";
 
 
-// function saveCurrentTime(e){
-//     const currentTime = e.seconds;
-//     localStorage.setItem(CURRENT_TIME_KEY, currentTime)
-// }
+const playerEl = document.getElementById('vimeo-player');
+const player = new Player(playerEl);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_KEY))
+
+
+function saveCurrentTime(e){
+    const currentTime = e.seconds;
+    localStorage.setItem(CURRENT_TIME_KEY, currentTime)
+}
 
 
